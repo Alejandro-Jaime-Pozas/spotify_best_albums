3716c3b65659eb230bc654c004f33fbe0dc44a57
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2023-07-31 22:46:08 -0600

    read.md

--- misc_scripts.py
+++ misc_scripts.py
@@ -1,9 +1,9 @@
 import requests
 
-r = requests.get('https://api.github.com/events')
+r = requests.get('https://www.rollingstone.com/music/music-lists/50-best-albums-of-2016-119690/')
 # returns response object
 print(r) 
 
 # payload = {'key1': 'value1', 'key2': 'value2'}
 # r = requests.get('https://httpbin.org/get', params=payload)
-print(r.links)
+print(r.content)
---
+++ openai.py
@@ -0,0 +1,1 @@
+api_key_hal = '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'
