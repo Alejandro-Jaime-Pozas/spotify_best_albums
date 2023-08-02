ee5a3bf5dbad65fe4b39175f12048c93d470a0da
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2023-08-01 21:01:00 -0600

    bs4

--- misc_scripts.py
+++ misc_scripts.py
@@ -1,9 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
 
-r = requests.get('https://www.rollingstone.com/music/music-lists/50-best-albums-of-2016-119690/')
-# returns response object
-print(r) 
 
-# payload = {'key1': 'value1', 'key2': 'value2'}
-# r = requests.get('https://httpbin.org/get', params=payload)
-print(r.content)
+# r = requests.get('https://www.rollingstone.com/music/music-lists/50-best-albums-of-2016-119690/')
+# # returns response object
+# print(r) 
+
+# # payload = {'key1': 'value1', 'key2': 'value2'}
+# # r = requests.get('https://httpbin.org/get', params=payload)
+# print(r.content)
+
+
+# Beautiful soup tutorial
+
