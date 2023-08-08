0f0e7feda853b83dab0c4a8d5bdf495e0a93ad72
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2023-08-07 20:55:55 -0600

    bs4 improvement

--- misc_scripts.py
+++ misc_scripts.py
@@ -6,8 +6,10 @@ r = requests.get('https://www.rollingstone.com/music/music-lists/50-best-albums-
 # print(r.content)
 
 
-# # Beautiful soup tutorial
-# doc = BeautifulSoup(r.text, "html.parser")
+# Beautiful soup tutorial
+doc = BeautifulSoup(r.text, "html.parser")
+pretty_text = doc.get_text()
 # text = doc.find_all("p")
-# print(text)
+# pretty_text = "\n\n".join([p.get_text() for p in text])
 
+print(pretty_text)
--- web_scraper.py
+++ web_scraper.py
@@ -2,19 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 
-url = "YOUR_WEBPAGE_URL_HERE"
+url = "https://www.rollingstone.com/music/music-lists/50-best-albums-of-2016-119690/"
 
 # Replace 'path_to_webdriver' with the actual path to your WebDriver executable (e.g., chromedriver)
-driver = webdriver.Chrome(executable_path='path_to_webdriver')
+driver = webdriver.Chrome('c:\Program Files (x86)\chromedriver.exe')
 driver.get(url)
 
 # Wait for the page to load (adjust the sleep time as needed)
 time.sleep(5)
 
-body_element = driver.find_element_by_tag_name('body')
-body_element.send_keys(Keys.CONTROL, 'a')
-time.sleep(1)  # Add a short delay to make sure the selection is completed
-copied_text = body_element.text
+# body_element = driver.find_element_by_tag_name('body')
+# body_element.send_keys(Keys.CONTROL, 'a')
+# time.sleep(1)  # Add a short delay to make sure the selection is completed
+# copied_text = body_element.text
+
+copied_text = driver.execute_script("return document.body.textContent;")
 
 print(copied_text)
 
