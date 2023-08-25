eaebbe658ec21abe61c1dad8daf5c5d55acabdcc
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2023-08-24 23:26:46 -0600

    added to main.py all code required, missing token automation

--- main.py
+++ main.py
@@ -7,34 +7,34 @@ from spotify import create_new_playlist, add_tracks_to_playlist
 year = 1954
 end_year = 2020
 
+# FOR EACH YEAR
+# for year in range(year, end_year):
 # get website's response content
-r = requests.get(f'https://www.acclaimedmusic.net/year/{year}a.htm')
+albums_by_year = requests.get(f'https://www.acclaimedmusic.net/year/{year}a.htm')
 # print(r.text)
 
-if r.status_code == 200:
+if albums_by_year.status_code == 200:
 
-    # Beautiful soup
-    soup = BeautifulSoup(r.text, "html.parser")#.prettify().encode('utf-8')
-
-    soup_str = ''.join([c for c in soup.prettify() if ord(c)<128]) # alphanum chars only
-
+    # CREATE Beautiful soup object
+    soup = BeautifulSoup(albums_by_year.text, "html.parser")#.prettify().encode('utf-8')
+    soup_str = ''.join([c for c in soup.prettify() if ord(c)<128]) # get alphanum chars only
     pattern = r'<td>\s*<a href="[^"]*">\s*(.*?)\s*</a>\s*</td>\s*<td>\s*<a href="[^"]*">\s*(.*?)\s*</a>\s*</td>'
     matches = re.findall(pattern, soup_str, re.DOTALL)
 
+    # CREATE LIST WITH ARTIST AND ALBUM FORMATTED FOR URL PARAMS
     artists_albums = []
-
     for match in matches:
         artist = re.sub(r'[^a-zA-Z0-9\s]', ' ', match[0]).replace(' amp ', ' ').strip().replace(' ', '%20')
         album = re.sub(r'[^a-zA-Z0-9\s]', ' ', match[1]).replace(' amp ', ' ').strip().replace(' ', '%20')
         artists_albums.append(f"{artist}%20{album}")
-    print(artists_albums)
+    # print(artists_albums)
 
-    # # CREATE A PLAYLIST FOR CURRENT YEAR
-    # playlist_id = create_new_playlist(year)
+    # CREATE A PLAYLIST FOR CURRENT YEAR
+    playlist_id = create_new_playlist(year)
 
-    # # FOR EACH ALBUM IN ARTISTS_ALBUMS SEARCH THE ALBUM AND ADD TRACKS TO NEW PLAYLIST
-    # for album in artists_albums:
-    #     add_tracks_to_playlist(playlist_id, album)
+    # FOR EACH ALBUM IN ARTISTS_ALBUMS SEARCH THE ALBUM AND ADD TRACKS TO NEW PLAYLIST
+    for album in artists_albums:
+        add_tracks_to_playlist(playlist_id, album)
 
 else:
     print(r.status_code)
