b1fcae22580507efac21c28cca459e5bc7cbbb9d
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2023-09-06 19:49:38 -0500

    completed spotify_keys module to get a new token using refresh token successfully

--- spotify_keys.py
+++ spotify_keys.py
@@ -1,4 +1,5 @@
 import requests
+
 # should remain the same, NO NEED TO UPDATE as long as oauth user has granted permission
 refresh_token = "AQCjPSNEq0pAsaqnbYF7webhgmJ9yIG2_5GGPZsttu2qJ9iS5_eM4KW5JOp_L_5OUNdjJzCl72dZhSv4JWkmAQ2BBw_ih6D4dJnHCfg9nH0aHZxxxevucIdYmNWl_Y-SHO0"
 
@@ -10,8 +11,6 @@ headers = {
   'Content-Type': 'application/x-www-form-urlencoded',
   'Cookie': '__Host-device_id=AQD50xdWI8NLcNIo85P1qYZ7A0YjanF65t6gaWSck1uDvcvK5XBHLTKwIDnj6E34KBGVF6lKw-xIVK_da9w_R-WUheu6-Vn4eCs; sp_tr=false'
 }
-response = requests.request("POST", url, headers=headers, data=payload)
-print(response.text)
 
 # store the token
 def get_token():
@@ -24,4 +23,4 @@ def get_token():
         print(response.status_code)
         return None 
 
-# print(get_token())
+print(get_token())
